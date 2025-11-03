3ef01c4fb80f03cf7ecd9cdc35135e557b138bf1
Willy Watson <wilibaldowatson@gmail.com>
2025-11-03 13:01:37 -0700

    Otra correccion

--- routes/square.js
+++ routes/square.js
@@ -15,12 +15,12 @@ router.post(
     try {
       const signature = req.headers["x-square-hmacsha256-signature"];
       const webhookSignatureKey = process.env.SQUARE_WEBHOOK_SIGNATURE_KEY;
-
+/*
       if (!signature || !webhookSignatureKey) {
         console.error("⚠️ Faltan datos de firma o clave de Square");
         return res.status(401).send("Faltan credenciales");
       }
-
+*/
       const rawBody = req.body.toString();
 
       // 🔐 Verificar firma HMAC
